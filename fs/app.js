7fdb4b6f5e61155676d65dcfa3cbce619e69ee66
ganeshdutt <gdutt2304@gmail.com>
2025-04-29 12:24:52 +0530

    File System

---
+++ fs/app.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+fs.readFile("file1.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
+//
+// fs.writeFile("file1.txt", "Hello ", (err) => {
+//   //   if (err) throw err;
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("File Created");
+// });
