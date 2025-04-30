1ef3cf6486c2d1e64eeca7de2b3974698cbbb51f
ganeshdutt <gdutt2304@gmail.com>
2025-04-30 12:18:15 +0530

    Data Update ,Delete

--- fs/code2.js
+++ fs/code2.js
@@ -13,3 +13,27 @@ const fs = require("fs");
 // } catch (err) {
 //   console.error(err);
 // }
+
+// fs.appendFile("example.txt", "\n New Data", (err) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("File Updated");
+// });
+
+// fs.rename("example.txt", "userInfo.txt", (err) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("File Renamed");
+// });
+
+fs.unlink("userInfo.txt", (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("File Deleted");
+});
