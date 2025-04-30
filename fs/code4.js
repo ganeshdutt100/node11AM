de48821d0f8dd2ee7e4248eead9d486108959055
ganeshdutt <gdutt2304@gmail.com>
2025-04-30 12:57:30 +0530

    writeStream

---
+++ fs/code4.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+// let writeStream = fs.createWriteStream("file3.txt");
+
+// writeStream.write("Hello world \n");
+// writeStream.write("Hello world 2 \n");
+// writeStream.write("Hello world 2 3 \n");
+// writeStream.end();
+
+// writeStream.on("finish", () => {
+//   console.log("Finished Data");
+// });
+// writeStream.on("error", (err) => {
+//   console.error("Error ");
+// });
