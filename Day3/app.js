e27818ae99c640c5152aa24ec89fbfb0d1467362
ganeshdutt <gdutt2304@gmail.com>
2025-05-05 12:44:10 +0530

    Modules

---
+++ Day3/app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const requestHandler = require("./user.js");
+
+const PORT = 3000;
+const server = http.createServer(requestHandler);
+server.listen(PORT, () => {
+  console.log(`Server is running on port at http://localhost:${PORT}`);
+});
+// function fun(){
+
+// }
+// fun()
