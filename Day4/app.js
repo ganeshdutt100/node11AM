834c79f9e82da92d24c0bafe989543fea20606e5
ganeshdutt <gdutt2304@gmail.com>
2025-05-07 12:27:14 +0530

    Update NPM

---
+++ Day4/app.js
@@ -0,0 +1,17 @@
+const http = require("http");
+let PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log("Request received: ", req.url, req.method);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(
+    `<h1>Hello, World!</h1><p>This is a simple Node.js application.</p> <p>hello</p> 
+     <h1>hello</h1>
+    `
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port at http://localhost:${PORT}`);
+});
