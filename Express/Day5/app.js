6bf6868c8b8147ef3f2cd466ce27b4b9c61a475b
ganeshdutt <gdutt2304@gmail.com>
2025-05-20 12:40:02 +0530

    Qr code

--- Express/Day5/app.js
+++ Express/Day5/app.js
@@ -4,9 +4,9 @@ import cors from "cors";
 const PORT = 3000;
 const app = express();
 
-// app.use()
+app.use(cors());
 
-app.get("/QrCode", (res, req) => {
+app.get("/QrCode", (req, res) => {
   let { url } = req.query;
   //   console.log(url);
   if (!url) return res.status(400).json({ error: "url is required" });
@@ -14,7 +14,7 @@ app.get("/QrCode", (res, req) => {
   const qr_image = qr.image(url, { type: "png" });
   const base64Image = `data:image/png;base64,${qr_image.toString("base64")}`;
 
-   res.json({qr :  base64Image})
+  res.json({ qr: base64Image });
 
   //   qr_image.pipe(res);
 });
