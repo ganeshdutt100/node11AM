b0adc8117bf2631b7a9e50c3cbc1744ba643e655
ganeshdutt <gdutt2304@gmail.com>
2025-05-20 12:12:21 +0530

    Update QrCode

---
+++ Express/Day5/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import qr from "qr-image";
+import cors from "cors";
+const PORT = 3000;
+const app = express();
+
+// app.use()
+
+app.get("/QrCode", (res, req) => {
+  let { url } = req.query;
+  //   console.log(url);
+  if (!url) return res.status(400).json({ error: "url is required" });
+
+  const qr_image = qr.image(url, { type: "png" });
+  const base64Image = `data:image/png;base64,${qr_image.toString("base64")}`;
+
+   res.json({qr :  base64Image})
+
+  //   qr_image.pipe(res);
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
