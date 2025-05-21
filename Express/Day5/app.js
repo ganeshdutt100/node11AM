ebd364f645afb2f21fe6e9a408381f953bd5df0a
ganeshdutt <gdutt2304@gmail.com>
2025-05-21 11:54:35 +0530

    Update QRCode

--- Express/Day5/app.js
+++ Express/Day5/app.js
@@ -1,24 +1,31 @@
 import express from "express";
 import qr from "qr-image";
 import cors from "cors";
+
 const PORT = 3000;
 const app = express();
-
 app.use(cors());
 
-app.get("/QrCode", (req, res) => {
-  let { url } = req.query;
-  //   console.log(url);
-  if (!url) return res.status(400).json({ error: "url is required" });
+app.get("/generate", (req, res) => {
+  try {
+    const { url } = req.query;
 
-  const qr_image = qr.image(url, { type: "png" });
-  const base64Image = `data:image/png;base64,${qr_image.toString("base64")}`;
+    console.log(url);
 
-  res.json({ qr: base64Image });
+    if (!url) return res.status(400).json({ error: "URL is required" });
 
-  //   qr_image.pipe(res);
+    const qr_image = qr.imageSync(url, { type: "png" });
+    const base64Image = `data:image/png;base64,${qr_image.toString("base64")}`;
+    res.json({ qr: base64Image });
+  } catch (err) {
+    next(err)
+  }
+});
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+  console.log(`✅ Server is running at http://localhost:${PORT}`);
 });
