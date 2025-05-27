076464c44bdbbfc1d1d75df05d994666fed51b31
Ganesh Dutt <gdutt2304@gmail.com>
2025-05-27 16:36:25 +0530

    OTP

--- Express/Day10/app.js
+++ Express/Day10/app.js
@@ -1,27 +1,25 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const path = require("path");
+const app = express();
 
-app.use(express.static(path.join(__dirname, "public"))); // 3000
+app.use(express.static(path.join(__dirname, "public")));
 
-function fun(length) {
+function otpGenerator(length) {
   let otp = "";
   for (let i = 0; i < length; i++) {
-    otp += Math.floor(Math.random() * 10); // Generate a random digit
+    otp += Math.floor(Math.random() * 10);
   }
-
   return otp;
 }
 
-//API Route
-app.get("/api/data", (req, res) => {
-  const length = parseInt(process.env.OTP_LENGTH) || 6; // Default to 6 if not set
-  const otp = fun(length);
+app.get("/api/otp", (req, res) => {
+  let length = parseInt(process.env.Value_length);
+  let otp = otpGenerator(length);
+
   res.json({ otp: otp });
-  //   res.send("hello world");
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Open http://localhost:${process.env.PORT} in your browser`);
 });
