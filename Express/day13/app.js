0cac6904d339a21a4b7e92c1da8d3d2027118916
Ganesh Dutt <gdutt2304@gmail.com>
2025-06-02 15:55:08 +0530

    ejs

--- Express/day13/app.js
+++ Express/day13/app.js
@@ -5,6 +5,12 @@ const port = 3004;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { name: "Aman" });
+  const user = {
+    name: "Aman",
+    age: 23,
+    occupation: "Software Engineer",
+    city: "Delhi",
+  };
+  res.render("index", { user });
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
