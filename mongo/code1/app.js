b7b22279f5c16c2a700e033869c534628acd92db
unknown <gdutt2304@gmail.com>
2025-06-23 17:28:28 +0530

    connection db

--- mongo/code1/app.js
+++ mongo/code1/app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const Message = require("./messageModel");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/insert-data", async (req, res) => {
+  try {
+    await Message.create({ text: "hello world" });
+    res.send("Data inserted successfully");
+  } catch (err) {
+    console.error("Error inserting data:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  const data = await Message.find();
+  res.json(data);
 });
 
 module.exports = app;
---
+++ mongo/code1/messageModel.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+const messageSchema = new mongoose.Schema({
+  text: String,
+});
+module.exports = mongoose.model("Message", messageSchema);
