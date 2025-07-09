8e439158b85343f542c187594fac4a63c5d556f5
unknown <gdutt2304@gmail.com>
2025-07-09 16:32:28 +0530

    login page

--- code3/app.js
+++ code3/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
-
+const User = require("./model/User");
 dotenv.config();
 
 app.set("view engine", "ejs");
@@ -35,12 +35,37 @@ app.post('/register' , async (req, res)=>{
  const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({name,email,password:hashedPassword});
 
+  const token  = jwt.sign({email:newUser.email}, process.env.JWT_SECRET);
+  res.cookie("token", token).send("Registered Successfully");
     
-    const user = await User.findOne({email});
+    // const user = await User.findOne({email});
 
 })
 
+app.post("/login" , async (req, res)=>{
+    const {email , password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        res.send("User not found");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        res.send("Password is incorrect");
+    }
+    const token  = jwt.sign({email:user.email}, process.env.JWT_SECRET);
+    res.cookie("token", token).send("Logged In Successfully");
+})
+
+
+app.get('/profile' , (req, res)=>{
+    const token = req.cookies.token;
+   if(!token) return res.send("Not logged in");
+
+   const user = jwt.verify(token, process.env.JWT_SECRET);
+   res.send("Logged in user "+ user.email);
+    
+})
 
 app.listen(process.env.PORT, () => {
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
